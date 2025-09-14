abd1d60a775ed0f3e3559f1c715b6d0d637a8db4
German Archila <106097211+4rchila@users.noreply.github.com>
2025-09-14 15:58:00 -0600

    detalles

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
 
-from hashing import Articulo, TablaHash
+from utils.hashing import Articulo, TablaHash
 
 hash_table = TablaHash()
 
